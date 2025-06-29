c30f4095f97b4476ee32a3b4fe47ce71eecb0626
RossiPatricio <benjacrowley@gmail.com>
2025-06-29 12:03:17 -0300

    Actualizacion 3 (29-6-2025)

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 ![EL-TORO WEBSITE](static/img/github-readme.png)
-
-# EL TORO – Personal Trainer Web
-…
---
+++ static/js/carousel.js
@@ -0,0 +1,29 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-btn.next');
+const prevButton = document.querySelector('.carousel-btn.prev');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// Ubica slides lado a lado
+slides.forEach((slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+});
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+};
+
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling || slides[slides.length - 1];
+  moveToSlide(track, currentSlide, prevSlide);
+});
+
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling || slides[0];
+  moveToSlide(track, currentSlide, nextSlide);
+});
